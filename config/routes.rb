d89d3f9e8584aa25269af28baf61e3f2ec36944c
lea cohen <cohenswim08@gmail.com>
2017-09-11 18:02:15 +0200

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  # devise_for :users
   root to: 'pages#home'
+  get '/search_results', to: 'pages#index'
+  get '/search_results/artist', to: 'pages#show'
+
+    resources :consumers, only: [:destroy, :index]
+    resources :consumer_events do
+      resources :artist_requests, only: [:create]
+    end
+
+
+
+    resources :artists do
+      resources :consumer_requests
+    end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
