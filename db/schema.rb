d2d33e987e21ed1688788afc7947c1a132d1723a
Sabrina John <sabrina.john1990@gmail.com>
2017-09-12 16:40:40 +0200

    changed home page search bar text color and fields size

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912101547) do
+ActiveRecord::Schema.define(version: 20170912141124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20170912101547) do
     t.integer  "user_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.string   "category"
     t.index ["user_id"], name: "index_artists_on_user_id", using: :btree
   end
 
