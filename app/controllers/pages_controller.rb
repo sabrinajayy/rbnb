e95267823d1e08f171de0c5208912ba39e63f10b
sam hinton <sam.hinton88@gmail.com>
2017-09-15 13:41:39 +0200

    fixed session logic in home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   def home
+
     @artists = Artist.all
+
+
+    if user_signed_in?
+      @user = current_user
+    end
+
   end
 
 end
