81cc6bd83d8a85b986eb8f1b3ac80e6d49c51378
lea cohen <cohenswim08@gmail.com>
2017-09-18 16:38:57 +0200

    finished reviews

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,8 +8,8 @@ class ArtistsController < ApplicationController
   end
 
   def show
-
     @review = Review.new
+    @reviews = Review.where(artist_id: @artist)
     requests = ConsumerRequest.where(artist: @artist)
     @confirmed = requests.select { |request| request.status == 'confirmed' }
     @unconfirmed = requests.select { |request| request.status == 'unconfirmed' }
@@ -81,7 +81,7 @@ class ArtistsController < ApplicationController
   end
 
   def artist_params
-    params.require(:artist).permit(:first_name, :last_name, :location, :tags, :travel_range, :instagram_handle, :category, :photo, :photo_cache, {artist_service: []})
+    params.require(:artist).permit(:first_name, :last_name, :bio, :location, :tags, :travel_range, :instagram_handle, :category, :photo, :photo_cache, {artist_service: []})
   end
 
 
