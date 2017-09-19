f4de5b3479ed635e3ad16c60a586dde4f5448149
Sabrina John <sabrina.john1990@gmail.com>
2017-09-19 15:38:29 +0200

    added swipebox plugin and artist image carrierwave gem to new artist profile form and display gallery images on artist profile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -55,6 +55,24 @@ $(function(){
 
     });
 
+;( function( $ ) {
+
+  $( '.swipebox' ).swipebox({
+    useCSS : true, // false will force the use of jQuery for animations
+    useSVG : true, // false to force the use of png for buttons
+    initialIndexOnArray : 0, // which image index to init when a array is passed
+    hideCloseButtonOnMobile : false, // true will hide the close button on mobile devices
+    removeBarsOnMobile : true, // false will show top bar on mobile devices
+    hideBarsDelay : 3000, // delay before hiding bars on desktop
+    videoMaxWidth : 1140, // videos max width
+    beforeOpen: function() {}, // called before opening
+    afterOpen: null, // called after opening
+    afterClose: function() {}, // called after closing
+    loopAtEnd: false // tru
+
+  });
+
+} )( jQuery );
 
 // end document ready function here
 });
