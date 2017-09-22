f17f7bff22e2920f892d0447d809f7a124d7398a
Sabrina John <sabrina.john1990@gmail.com>
2017-09-22 16:02:31 +0200

    changed status of quotes based on button click

--- db/seeds.rb
+++ db/seeds.rb
@@ -65,7 +65,9 @@ ArtistService.create(name: "Wedding Day Service",price: 250.0,artist: artist_sar
 ArtistService.create(name: "Lesson", price: 50.0,artist: artist_sarah)
 puts
 
+
 puts "Creating and artist called Angelo"
+user = User.create({ email: 'angelo@angelo.com', password: 'password' })
 artist_angelo = Artist.create({ user: user,
                          first_name: 'Angelo',
                          last_name: 'Cervantes',
