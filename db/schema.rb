1d466b727c7e1722914b73acc098fd5fbaf8d39a
Cam Weiss <bb.cweiss@gmail.com>
2017-09-22 12:41:47 +0200

    updated footer layout and infocard padding

--- db/schema.rb
+++ db/schema.rb
@@ -162,7 +162,6 @@ ActiveRecord::Schema.define(version: 20170921183752) do
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
     t.datetime "end_date"
-    t.string   "type"
     t.string   "category"
     t.index ["artist_id"], name: "index_time_blocks_on_artist_id", using: :btree
     t.index ["consumer_request_id"], name: "index_time_blocks_on_consumer_request_id", using: :btree
