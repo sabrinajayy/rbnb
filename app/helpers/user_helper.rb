73be2c1a2ab33d9e703474b933076900d0f3fd84
lea cohen <cohenswim08@gmail.com>
2017-09-25 15:15:13 +0200

    fixed user helper

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,9 +1,9 @@
 module UserHelper
   def display_name
      if current_user.is_artist?
-      [@consumer_request.artist.first_name, @consumer_request.artist.last_name].join(' ')
-     else
       [@consumer_request.consumer.first_name, @consumer_request.consumer.last_name].join(' ')
+     else
+      [@consumer_request.artist.first_name, @consumer_request.artist.last_name].join(' ')
      end
   end
 
